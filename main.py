9e648b228ace100fbfc4207d20c5ba0e55f3ff1c
decalte <decalte@icloud.com>
2026-09-11 17:46:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -891,18 +891,23 @@ class PrivateRoomPanelView(discord.ui.LayoutView):
         super().__init__(timeout=None)
 
         container = discord.ui.Container(
-            discord.ui.TextDisplay(
-                "## Управление приватной комнатой\n\n"
-                "Здесь Вы можете управлять своей приватной комнатой.\n"
-                "Используйте разделы ниже, чтобы изменить её настройки и управлять участниками."
-            ),
-            discord.ui.Separator(),
-            discord.ui.ActionRow(
-                PrivateRoomSettingsSelect(),
-            ),
-            discord.ui.ActionRow(PrivateRoomMemberActionsSelect()),
-            accent_color=COLOR,
-        )
+    discord.ui.TextDisplay("-# Приватные комнаты"),
+    discord.ui.TextDisplay(
+        "## Управление приватной комнатой"
+    ),
+    discord.ui.Separator(),
+    discord.ui.TextDisplay(
+        "Здесь Вы можете управлять своей приватной комнатой.\n"
+        "Используйте разделы ниже, чтобы изменить её настройки и управлять участниками."
+    ),
+    discord.ui.ActionRow(
+        PrivateRoomSettingsSelect(),
+    ),
+    discord.ui.ActionRow(
+        PrivateRoomMemberActionsSelect()
+    ),
+    accent_color=COLOR,
+)
         self.add_item(container)
 
 
